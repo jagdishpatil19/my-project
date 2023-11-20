a103f5a19717dd9e362c2ee438bf6260e6f4d14f
jagdishpatil19 <patiljagdish471@gmail.com>
2023-11-20 19:28:04 +0530

    all files added

---
+++ You Tube page/you-tube.js
@@ -0,0 +1,14 @@
+//  document.querySelector('#menu').addEventListener('click',function (e){
+//     let button=(e.target)
+     
+//  })
+
+let sideBar=document.querySelector('.side-bar')
+
+function show(){
+    sideBar.style.display="block";
+}
+
+function hide(){
+    sideBar.style.display="none";
+}
